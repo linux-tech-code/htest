4af992548bb4502cb1a96e9ef78cbcb08eefb2bd
linux-tech-code <63151914+linux-tech-code@users.noreply.github.com>
2020-08-20 07:59:36 +0700

    Add files via upload

---
+++ kieudulieu.py
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     25/06/2020
+# Copyright:   (c) Administrator 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a = 9
+
+ # Output: <class 'int'>
+print(type(a))
+
+ # Output: <class 'float'>
+print(type(5.0))
+
+ # Output: (10+2j)
+b = 8 + 2j
+print(b + 2)
+
+ # Kiểm tra xem b có phải là số phức không
+ # Output: True
+print(isinstance(b, complex))
+
+#chuyển đổi các kiểu dữ liệu
+a=2
+b=5.0
+print (a+b)
+
+
+x=int(3.6)
+y=float(8)
+print (x)
+print (y)
+
